bf912f7152c4eb68da3d508f8e46e6ff74409e57
mtotodev05 <mtotodev05@gmail.com>
2018-12-30 17:38:21 +0300

    Setup server running

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const firebaseAdmin = require('firebase-admin');
+const serviceAccount = require('./api/config/my-portal-e90f4-firebase-adminsdk-d3oav-f0384ba801');
+const app = express();
+
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if(req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods','PUT,PATCH,POST,DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function(req,res,next){
+    const error = new Error('Route Not Found');
+    error.status = 404;
+    next(error);
+});
+app.use(function(err,req,res){
+    res.status(err.status || 500);
+    res.json({
+        err:{
+            message:err.message
+        }
+    })
+});
+
+
+module.exports = app;
