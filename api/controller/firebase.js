23efc96e3d6835008fb9b367a46ff51e09b19980
mtotodev05 <mtotodev05@gmail.com>
2018-12-31 13:21:05 +0300

    Format code, include after() on every test description

--- api/controller/firebase.js
+++ api/controller/firebase.js
@@ -1,9 +1,9 @@
 const firebaseAdmin = require('firebase-admin');
 
-exports.sendNotifications = function(req,res,next){
+exports.sendNotifications = function (req, res, next) {
     let _this = req.body;
 
-    if(!(_this.messageBody && _this.messageTitle && _this.messageTopic)){
+    if (!(_this.messageBody && _this.messageTitle && _this.messageTopic)) {
         return res.status(400).send({
             message: 'Invalid Request Body'
         })
@@ -14,33 +14,33 @@ exports.sendNotifications = function(req,res,next){
     messageBody = _this.messageBody;
     messageTopic = _this.messageTopic;
 
-  let androidNotification = {
-    android:{
-        ttl: 3600 *1000,
-        priority: 'normal',
-        notification: {
-            title: messageTitle,
-            body: messageBody,
-            icon: 'stock_ticker_update',
-            color: '#1D1124'
-        }
-    },
-      topic: messageTopic
-  };
-  firebaseAdmin.messaging().send(androidNotification)
-      .then(response=>{
-          return res.status(200).send({
-              message:'Message Sent Successfully',
-              messageBody:androidNotification,
-              response
-          })
-      })
-      .catch(error=>{
-          console.log(error.message);
-         return res.status(500)
-             .send({
-                 message:'Error Occured While Sending Message',
-                 error: error.message
-             })
-      });
+    let androidNotification = {
+        android: {
+            ttl: 3600 * 1000,
+            priority: 'normal',
+            notification: {
+                title: messageTitle,
+                body: messageBody,
+                icon: 'stock_ticker_update',
+                color: '#1D1124'
+            }
+        },
+        topic: messageTopic
+    };
+    firebaseAdmin.messaging().send(androidNotification)
+        .then(response => {
+            return res.status(200).send({
+                message: 'Message Sent Successfully',
+                messageBody: androidNotification,
+                response
+            })
+        })
+        .catch(error => {
+            console.log(error.message);
+            return res.status(500)
+                .send({
+                    message: 'Error Occured While Sending Message',
+                    error: error.message
+                })
+        });
 };
--- api/test/index.js
+++ api/test/index.js
@@ -1,66 +1,69 @@
-const request  = require('supertest');
+const request = require('supertest');
 const expect = require('expect');
 const server = require('../../server');
 let assert = require('chai').assert;
 
-describe('/Testing API Calls',()=>{
-   var token = null;
-   var tokenRequestBody = {
-     email: 'ditadev@daystar.ac.ke',
-     uid : '885ffefef'
-   };
-   before(function(done){
-       request(server)
-           .post('/api/v1/client/access-token')
-           .send(tokenRequestBody)
-           .end(function(err,res){
-              if(!err){
-                  token = res.body.token.accessToken;
-                  done();
-              }
-           });
-
-   });
-   it('it should not post data with invalid request body',(done)=>{
-     setTimeout(done,1000);
-     let invalidNotificationBody = {
-       messageTopic: 'debug',
-       messageTitle: 'test'
-       //The Message Body Left Out Intentionally
-     };
-     request(server)
-         .post('/api/v1/send')
-         .set('Authorization', 'Bearer ' + token)
-         .send(invalidNotificationBody)
-         .expect(400)
-         .end((err,res)=>{
-             if(err){
-                  throw err;
-             }
-             done();
-         })
-   });
-});
-describe('/Should Post Correct Data',()=>{
+describe('/Testing API Calls', () => {
     var token = null;
     var tokenRequestBody = {
         email: 'ditadev@daystar.ac.ke',
-        uid : '885ffefef'
+        uid: '885ffefef'
     };
-    before(function(done){
+    before(function (done) {
         request(server)
             .post('/api/v1/client/access-token')
             .send(tokenRequestBody)
-            .end(function(err,res){
-                if(!err){
+            .end(function (err, res) {
+                if (!err) {
                     token = res.body.token.accessToken;
                     done();
                 }
             });
 
     });
-    it('it should send notification on valid request body',(done)=>{
-        setTimeout(done,1000);
+    it('it should not post data with invalid request body', (done) => {
+        setTimeout(done, 1000);
+        let invalidNotificationBody = {
+            messageTopic: 'debug',
+            messageTitle: 'test'
+            //The Message Body Left Out Intentionally
+        };
+        request(server)
+            .post('/api/v1/send')
+            .set('Authorization', 'Bearer ' + token)
+            .send(invalidNotificationBody)
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    throw err;
+                }
+                done();
+            })
+    });
+    after(function () {
+        server.close();
+    });
+});
+describe('/Should Post Correct Data', () => {
+    var token = null;
+    var tokenRequestBody = {
+        email: 'ditadev@daystar.ac.ke',
+        uid: '885ffefef'
+    };
+    before(function (done) {
+        request(server)
+            .post('/api/v1/client/access-token')
+            .send(tokenRequestBody)
+            .end(function (err, res) {
+                if (!err) {
+                    token = res.body.token.accessToken;
+                    done();
+                }
+            });
+
+    });
+    it('it should send notification on valid request body', (done) => {
+        setTimeout(done, 1000);
         let validRequestBody = {
             messageTopic: 'debug',
             messageTitle: 'test',
@@ -71,32 +74,35 @@ describe('/Should Post Correct Data',()=>{
             .set('Authorization', 'Bearer ' + token)
             .send(validRequestBody)
             .expect(200)
-            .end((err,res)=>{
-                if(err){
+            .end((err, res) => {
+                if (err) {
                     throw err;
                 }
                 done();
             })
-    })
+    });
+    after(function () {
+        server.close();
+    });
 });
-describe('/Non-Existent Endpoints',()=>{
-   it('it should reject non-existent endpoints',(done)=>{
-     setTimeout(done,1000);
-     request(server)
-         .get('/api/v1/send')
-         .expect(404)
-         .end((err,res)=>{
-             if(err){
-                 done();
-                 throw err;
-             }
+describe('/Non-Existent Endpoints', () => {
+    it('it should reject non-existent endpoints', (done) => {
+        setTimeout(done, 1000);
+        request(server)
+            .get('/api/v1/send')
+            .expect(404)
+            .end((err, res) => {
+                if (err) {
+                    done();
+                    throw err;
+                }
 
-             done();
-         })
-   });
+                done();
+            })
+    });
 
-   after(function () {
-       server.close();
-   })
+    after(function () {
+        server.close();
+    });
 });
 
