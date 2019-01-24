64aa4c4c554a5357148a1fc93f9e457b0577cc03
mtotodev05 <mtotodev05@gmail.com>
2019-01-24 12:59:44 +0300

    Update test

--- api/test/index.js
+++ api/test/index.js
@@ -66,12 +66,50 @@ describe('/Non-Existent Endpoints', () => {
 });
 
 describe('/Fetch All Notifications', () => {
-
-    it('it should fetch all push Notifcations', (done) => {
-
+    it('it should fetch all push Notifications', (done) => {
         request(server)
             .get('/api/v1/fetch-all')
             .set('Authorization', 'Bearer ' + accessToken)
             .expect(200, done);
     })
 });
+
+describe('Fetch Notification With Email', () => {
+    let requestBody = {
+        emailAddress: 'mtotodev05@gmail.com'
+    };
+    it('it should fetch all push notifications by email addresses', (done) => {
+        request(server)
+            .post('/api/v1/fetch-by-email')
+            .set('Authorization', 'Bearer ' + accessToken)
+            .send(requestBody)
+            .expect(200, done);
+    });
+});
+
+describe('Should return null on non-existent email addresses', () => {
+    let requestBody = {
+        emailAddress: 'kamaubrian@gmail.com'
+    };
+    it('it should return 404 on null push notifications', (done) => {
+        request(server)
+            .post('/api/v1/fetch-by-email')
+            .set('Authorization', 'Bearer ' + accessToken)
+            .send(requestBody)
+            .expect(404, done);
+    })
+});
+
+describe('should not send on invalid request body ', () => {
+    let requestBody = {
+        emailAddres: 'mtotodev05@gmail.com'
+    };
+    it('it should invalid request body error', (done) => {
+        request(server)
+            .post('/api/v1/fetch-by-email')
+            .set('Authorization', 'Bearer ' + accessToken)
+            .send(requestBody)
+            .expect(401, done);
+    })
+});
+
