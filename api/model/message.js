cfc2b4410f127e3c6c4d9c319655aa50e1a7665b
mtotodev05 <mtotodev05@gmail.com>
2019-01-24 10:06:59 +0300

    Push Notification model

---
+++ api/model/message.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const messageSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    emailAddress: {type: String, required: true},
+    messageTitle: {type: String, required: true},
+    messageBody: {type: String, required: true},
+    messageTopic: {type: String, required: true}
+
+}, {timestamps: true});
+
+module.exports = mongoose.model('Message', messageSchema);
