32ff66d37088b44cac27abe72d9667d31857c29f
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-19 15:10:08 +0300

    feat: Add outlook integration [WIP]
    This commit adds a webhook for outlook emails.

--- api/controller/outlook.js
+++ api/controller/outlook.js
@@ -1,6 +1,15 @@
+// const Busboy = require('busboy');
+const asyncBusboy = require('./async-busybox');
+
 exports.webHook = async (req, res, next) => {
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
     console.log('hook request received');
-    console.log(req.body);
-    console.log(req.files);
+    const {files, fields} = await asyncBusboy(req);
+    const data = JSON.parse(fields['data']);
+    console.log(data);
+    console.log(files);
+    // TODO Do data processing...
     res.json({'message': 'Success'});
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -7,15 +7,19 @@ const {isTokenValid} = require('../auth/auth');
 const excelController = require('../controller/excel');
 const unitsController = require('../controller/units');
 const outlookController = require('../controller/outlook');
+const { fileParser } = require('express-multipart-file-parser')
 const basicAuth = require('express-basic-auth');
-const bodyParser = require('body-parser');
-
+const rawBodyOptions = {
+    rawBodyOptions: {
+      limit: '8mb',
+    }
+}
 router.post('/client/access-token', userAuth.generateAccessToken);
 
 router.post('/send', isTokenValid, firebaseAdminController.sendNotifications);
 router.get('/fetch-all', isTokenValid, notificationController.fetchPushNotifications);
 router.post('/fetch-by-email', isTokenValid, notificationController.fetchNotificationByEmailAddress);
-router.post('/excel/upload', isTokenValid, excelController.uploadExcelFile);
+router.post('/excel/upload', isTokenValid, fileParser(rawBodyOptions), excelController.uploadExcelFile);
 router.get('/units', unitsController.getUnits);
 router.post('/hooks', basicAuth({
     users: { 'admin': 'supersecret' }
