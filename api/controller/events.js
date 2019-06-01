49d57548426040ab6ab48d9fbe705b3f166452e8
mtotodev05 <mtotodev05@gmail.com>
2019-06-01 20:33:20 +0300

    feat: fetching events

--- api/controller/events.js
+++ api/controller/events.js
@@ -1,14 +1,30 @@
 const calendarEventsModel = require('../model/calendar-events');
-
+const moment = require('moment');
 exports.fetchEvents = async (req, res, next) => {
-    try{
-       let eventsHandler = await calendarEventsModel.fetchCalendarEvents();
-       return res.status(200).send({
-           body:{
-               eventsHandler
-           }
-       })
-    }catch (e) {
+    try {
+        const fetchedEvents = await calendarEventsModel.fetchCalendarEvents();
+        let fetched = [];
+        if (fetchedEvents.empty) {
+            return res.status(404).send({
+                message: 'No Calendar events found in the Database'
+            })
+        }
+        fetchedEvents.forEach(documents => {
+            fetched.push(documents.data());
+        });
+        console.log(fetched);
+        const parsedEvents = await fetched.map(event => {
+            return {
+                description: event.description,
+                startDate: moment(event.start_date.startDate).format('DD/MM/YYYY HH:mm'),
+                endDate: moment(event.end_date).format("DD/MM/YYYY HH:mm")
+            }
+        })
+        return res.status(200).send({
+            message: 'Fetched Calendar Events',
+            events: parsedEvents
+        })
+    } catch (e) {
         return res.status(500).send({
             message: 'Internal Server Error',
             error: e.message
--- api/model/calendar-events.js
+++ api/model/calendar-events.js
@@ -2,16 +2,38 @@ const database = require('./abstract.database');
 let parentCollection = database.collection('calendar');
 
 exports.fetchCalendarEvents = async () => {
-    let references = parentCollection.doc('dCGAqIFz6LHNhpBcisqX');
-    references.getCollections().then(collections => {
-        collections.forEach(async collection => {
-            console.log("Found subcollection: ",collection.id);
+    /*   let documentArray = [];
+       let eventsArray = [];
+       let parentDocuments = await parentCollection.get();
+       parentDocuments.forEach(documents =>{
+           documentArray.push(documents.id); //Events id, found on the first id.
+       })
+       let references = parentCollection.doc(documentArray[0]);
+       references.getCollections().then(collections => {
+           collections.forEach(async collection => {
+               console.log("Found subcollection: ",collection.id);
+               let documents = await collection.get();
+               console.log(documents);
+               documents.forEach(doc=>{
+                   console.log(doc.data())
+                   return eventsArray.push(doc.data());
+               })
+           })
+       })*/
+    try {
+        let documentsArray = [];
+        let fetchedParentDocuments = await parentCollection.get();
+        fetchedParentDocuments.forEach(documents => {
+            documentsArray.push(documents.id);
+        });
+        let eventsParentDocument = parentCollection.doc(documentsArray[0]);
+        let eventSubCollection = await eventsParentDocument.getCollections();
+        /*let collection = await eventSubCollection.forEach(async subCollection => {
+            let documents = await subCollection.get();
+        });*/
+        return await eventSubCollection[0].get();
 
-            let documents = await collection.get();
-            console.log(documents);
-            documents.forEach(doc=>{
-                console.log(doc.data())
-            })
-        })
-    })
+    } catch (e) {
+        console.error("Error Fetching Calendar Events", e.message);
+    }
 }
